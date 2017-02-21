1b5591998077b3783e1ffd41c3cf5832c5d13a0d
Alessandro Rizzotto <easysoft2k15@gmail.com>
2017-02-21 17:19:26 +0100

    Use  webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports=
     entry:  path.resolve("dev_js","index.jsx"),
     output: 
     {
-        path: path.resolve("output"),
+        path: path.resolve(__dirname, "output"),
+        publicPath: "/output",
         filename: "bundle.js"
     },
     module:
